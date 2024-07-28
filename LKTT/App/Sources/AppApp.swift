ba585614c7fcc62501e1e4817392c77b8c5cc28d
Asio Otus <asio.otus.verus@icloud.com>
2024-07-28 13:16:18 +0300

    Initial commit

---
+++ LKTT/App/Sources/AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppApp.swift
+//  App
+//
+//  Created by Anton on 28/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct AppApp: App {
+	var body: some Scene {
+		WindowGroup {
+			ContentView()
+		}
+	}
+}
