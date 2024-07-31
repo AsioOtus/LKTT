8eb90d1226153075bea3b352de5b0cbae4270c1d
Asio Otus <asio.otus.verus@icloud.com>
2024-07-31 18:50:16 +0300

    Fix packages dependencies

--- LKTT/Packages/ILPackages/Screens/il-screen-photo-details/Package.swift
+++ LKTT/Packages/ILPackages/Screens/il-screen-photo-details/Package.swift
@@ -16,6 +16,9 @@ let package = Package(
 		)
 	],
 	dependencies: [
+		.package(path: "../../../DLPackages/dl-use-cases"),
+		.package(path: "../../../DLPackages/dl-use-cases-protocols"),
+
 		.package(url: "https://github.com/AsioOtus/multitool", from: "1.0.0"),
 		.package(url: "https://github.com/pointfreeco/swift-dependencies", exact: "1.3.0"),
 	],
@@ -23,6 +26,9 @@ let package = Package(
 		.target(
 			name: "ILScreenPhotoDetails",
 			dependencies: [
+				.product(name: "DLUseCases", package: "dl-use-cases"),
+				.product(name: "DLUseCasesProtocols", package: "dl-use-cases-protocols"),
+
 				.product(name: "Multitool", package: "multitool"),
 				.product(name: "Dependencies", package: "swift-dependencies"),
 			]
